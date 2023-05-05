2ca8889f2af212231abe920296f022453c98ccf4
Roddie Kieley <rkieley@redhat.com>
2023-05-05 06:36:37 -0230

    issue67: Converted tabs to spaces.

--- PlayerShip.cs
+++ PlayerShip.cs
@@ -71,253 +71,253 @@ public partial class PlayerShip : CharacterBody2D
 
   public GameEvent CreatePlayerGameEventBuffer(GameEvent.GameEventType eventType)
   {
-	//EntityGameEventBuffer egeb = new EntityGameEventBuffer();
-	GameEvent gameEvent = new GameEvent();
+    //EntityGameEventBuffer egeb = new EntityGameEventBuffer();
+    GameEvent gameEvent = new GameEvent();
 
-	//egeb.Type = BufferType;
-	gameEvent.game_event_type = eventType;
+    //egeb.Type = BufferType;
+    gameEvent.game_event_type = eventType;
 
-	//egeb.objectType = EntityGameEventBuffer.EntityGameEventBufferObjectType.Player;
-	gameEvent.game_object_type = GameEvent.GameObjectType.GameObjectTypePlayer;
+    //egeb.objectType = EntityGameEventBuffer.EntityGameEventBufferObjectType.Player;
+    gameEvent.game_object_type = GameEvent.GameObjectType.GameObjectTypePlayer;
 
-	//egeb.Uuid = uuid;
-	gameEvent.Uuid = uuid;
+    //egeb.Uuid = uuid;
+    gameEvent.Uuid = uuid;
 
-	//Box2d.PbBody body = new Box2d.PbBody();
-	//body.Type = Box2d.PbBodyType.Kinematic; // not sure if this should maybe be static
+    //Box2d.PbBody body = new Box2d.PbBody();
+    //body.Type = Box2d.PbBodyType.Kinematic; // not sure if this should maybe be static
 
-	// need to use the GlobalPosition because the ship node ends up being offset
-	// from the parent Node2D
-	//body.Position = new Box2d.PbVec2 
-	//  { 
-	//    X = GlobalPosition.x,
-	//    Y = GlobalPosition.y
-	//  };
-	gameEvent.PositionX = (int)GlobalPosition.X;
-	gameEvent.PositionY = (int)GlobalPosition.Y;
+    // need to use the GlobalPosition because the ship node ends up being offset
+    // from the parent Node2D
+    //body.Position = new Box2d.PbVec2 
+    //  { 
+    //    X = GlobalPosition.x,
+    //    Y = GlobalPosition.y
+    //  };
+    gameEvent.PositionX = (int)GlobalPosition.X;
+    gameEvent.PositionY = (int)GlobalPosition.Y;
 
-	//body.Angle = RotationDegrees;
-	gameEvent.Angle = RotationDegrees;
+    //body.Angle = RotationDegrees;
+    gameEvent.Angle = RotationDegrees;
 
-	//body.AbsoluteVelocity = CurrentVelocity;
-	gameEvent.AbsoluteVelocity = (float)CurrentVelocity;
-	
-	gameEvent.HitPoints = HitPoints;
+    //body.AbsoluteVelocity = CurrentVelocity;
+    gameEvent.AbsoluteVelocity = (float)CurrentVelocity;
+    
+    gameEvent.HitPoints = HitPoints;
 
-	return gameEvent;
+    return gameEvent;
   }
 
   public void ExpireMissile() 
   { 
-	_serilogger.Verbose($"PlayerShip.cs: removing missile {MyMissile.uuid} belongs to {MyMissile.MyPlayer.uuid}");
-	MyServer.RemoveMissile(MyMissile);
-	MyMissile = null;
+    _serilogger.Verbose($"PlayerShip.cs: removing missile {MyMissile.uuid} belongs to {MyMissile.MyPlayer.uuid}");
+    MyServer.RemoveMissile(MyMissile);
+    MyMissile = null;
   }
 
   public void FireMissile(string missileUUID = null)
   {
-	// only one missile allowed for now
-	if (MyMissile != null) 
-	{ 
-	  _serilogger.Debug($"PlayerShip.cs: Missile for player {uuid} exists - skipping");
-	  return; 
-	}
+    // only one missile allowed for now
+    if (MyMissile != null) 
+    { 
+      _serilogger.Debug($"PlayerShip.cs: Missile for player {uuid} exists - skipping");
+      return; 
+    }
 
-	// check if reload complete
-	if (MissileReady == false)
-	{
-	  _serilogger.Debug($"PlayerShip.cs: player {uuid} not done with reload - skipping");
-	  return;
-	}
+    // check if reload complete
+    if (MissileReady == false)
+    {
+      _serilogger.Debug($"PlayerShip.cs: player {uuid} not done with reload - skipping");
+      return;
+    }
 
-	Node missileNode = MissilePackedScene.Instantiate();
-	MyMissile = (SpaceMissile)missileNode;
+    Node missileNode = MissilePackedScene.Instantiate();
+    MyMissile = (SpaceMissile)missileNode;
 
-	// TODO: need to check for UUID collision
-	_serilogger.Debug($"PlayerShip.cs: Supplied UUID is {missileUUID}");
-	if (missileUUID != null)
-	  // use the suggested UUID
-	  { MyMissile.uuid = missileUUID; }
-	else
-	  { MyMissile.uuid = Guid.NewGuid().ToString(); }
+    // TODO: need to check for UUID collision
+    _serilogger.Debug($"PlayerShip.cs: Supplied UUID is {missileUUID}");
+    if (missileUUID != null)
+      // use the suggested UUID
+      { MyMissile.uuid = missileUUID; }
+    else
+      { MyMissile.uuid = Guid.NewGuid().ToString(); }
 
-	_serilogger.Debug($"PlayerShip.cs: Missile UUID is {MyMissile.uuid}");
+    _serilogger.Debug($"PlayerShip.cs: Missile UUID is {MyMissile.uuid}");
 
-	// missile should point in the same direction as the ship
-	MyMissile.Rotation = Rotation;
-	
-	// TODO: need to offset this to the front of the ship
-	// start at our position
-	MyMissile.Position = GlobalPosition;
+    // missile should point in the same direction as the ship
+    MyMissile.Rotation = Rotation;
+    
+    // TODO: need to offset this to the front of the ship
+    // start at our position
+    MyMissile.Position = GlobalPosition;
 
-	// negative direction is "up"
-	Vector2 offset = new Vector2(0, -60);
+    // negative direction is "up"
+    Vector2 offset = new Vector2(0, -60);
 
-	// rotate the offset to match the current ship heading
-	offset = offset.Rotated(Rotation);
-	MyMissile.Position = MyMissile.Position + offset;
+    // rotate the offset to match the current ship heading
+    offset = offset.Rotated(Rotation);
+    MyMissile.Position = MyMissile.Position + offset;
 
-	// set missile's parameters based on current modifiers
-	MyMissile.MissileSpeed = MissileSpeed;
-	MyMissile.MissileLife = MissileLife;
-	MyMissile.MissileDamage = MissileDamage;
+    // set missile's parameters based on current modifiers
+    MyMissile.MissileSpeed = MissileSpeed;
+    MyMissile.MissileLife = MissileLife;
+    MyMissile.MissileDamage = MissileDamage;
 
-	// this is a poop way to do this
-	MyMissile.MyPlayer = this;
+    // this is a poop way to do this
+    MyMissile.MyPlayer = this;
 
-	// send the missile creation message
-	_serilogger.Debug($"PlayerShip.cs: creating missile {MyMissile.uuid} belongs to {MyMissile.MyPlayer.uuid}");
-	Node rootNode = GetNode<Node>("/root");
-	rootNode.AddChild(MyMissile);
-	MyServer.InstantiateMissile(MyMissile);
+    // send the missile creation message
+    _serilogger.Debug($"PlayerShip.cs: creating missile {MyMissile.uuid} belongs to {MyMissile.MyPlayer.uuid}");
+    Node rootNode = GetNode<Node>("/root");
+    rootNode.AddChild(MyMissile);
+    MyServer.InstantiateMissile(MyMissile);
 
-	// set the reload countdown
-	MissileReloadCountdown = MissileReloadTime;
-	MissileReady = false;
+    // set the reload countdown
+    MissileReloadCountdown = MissileReloadTime;
+    MissileReady = false;
   }
 
   // Called when the node enters the scene tree for the first time.
   public override void _Ready()
   {
-	// initialize the logging configuration
-	MyServer = GetNode<Server>("/root/Server");
-	_serilogger = MyServer._serilogger;
+    // initialize the logging configuration
+    MyServer = GetNode<Server>("/root/Server");
+    _serilogger = MyServer._serilogger;
 
-	Node2D shipThing = (Node2D)GetParent();
-	Label playerIDLabel = (Label)shipThing.GetNode("Stat/IDLabel");
+    Node2D shipThing = (Node2D)GetParent();
+    Label playerIDLabel = (Label)shipThing.GetNode("Stat/IDLabel");
 
-	// TODO: deal with really long UUIDs
-	playerIDLabel.Text = uuid;
+    // TODO: deal with really long UUIDs
+    playerIDLabel.Text = uuid;
 
-	myCamera = GetNode<Camera2D>("Camera2D");
-	shipSprite = GetNode<Sprite2D>("Sprite2D");
+    myCamera = GetNode<Camera2D>("Camera2D");
+    shipSprite = GetNode<Sprite2D>("Sprite2D");
 
-	// TODO: we are doing instant rotation so probably should rename this
-	angularVelocityLabel = (Label)shipThing.GetNode("Stat/AngularVelocity");
-	linearVelocityLabel = (Label)shipThing.GetNode("Stat/LinearVelocity");
-	hitPointsLabel = (Label)shipThing.GetNode("Stat/HitPoints");
-	positionLabel = (Label)shipThing.GetNode("Stat/Position");
-	hexLabel = (Label)shipThing.GetNode("Stat/Hex");
+    // TODO: we are doing instant rotation so probably should rename this
+    angularVelocityLabel = (Label)shipThing.GetNode("Stat/AngularVelocity");
+    linearVelocityLabel = (Label)shipThing.GetNode("Stat/LinearVelocity");
+    hitPointsLabel = (Label)shipThing.GetNode("Stat/HitPoints");
+    positionLabel = (Label)shipThing.GetNode("Stat/Position");
+    hexLabel = (Label)shipThing.GetNode("Stat/Hex");
 
-	theLayout = MyServer.HexLayout;
+    theLayout = MyServer.HexLayout;
   }
 
   public void TakeDamage(int Damage)
   {
-	_serilogger.Debug($"PlayerShip.cs: {uuid}: Taking damage: {Damage}");
-	HitPoints -= Damage;
-	_serilogger.Debug($"PlayerShip.cs: {uuid}: Hitpoints: {HitPoints}");
+    _serilogger.Debug($"PlayerShip.cs: {uuid}: Taking damage: {Damage}");
+    HitPoints -= Damage;
+    _serilogger.Debug($"PlayerShip.cs: {uuid}: Hitpoints: {HitPoints}");
 
-	if (HitPoints <= 0)
-	{
-	  _serilogger.Debug($"PlayerShip.cs: Hitpoints zeroed for {uuid}! Remove the player!");
-	  QueuedForRemoval = true;
-	  RemovePlayer();
-	}
+    if (HitPoints <= 0)
+    {
+      _serilogger.Debug($"PlayerShip.cs: Hitpoints zeroed for {uuid}! Remove the player!");
+      QueuedForRemoval = true;
+      RemovePlayer();
+    }
   }
 
   void RemovePlayer()
   {
-	_serilogger.Debug($"PlayerShip.cs: Enqueuing player removal: {uuid}");
-	MyServer.PlayerRemoveQueue.Enqueue(uuid);
+    _serilogger.Debug($"PlayerShip.cs: Enqueuing player removal: {uuid}");
+    MyServer.PlayerRemoveQueue.Enqueue(uuid);
   }
 
   void CheckMissileReload(double delta)
   {
-	// nothing to check if we are already reloaded
-	if (MissileReady == true) { return; }
+    // nothing to check if we are already reloaded
+    if (MissileReady == true) { return; }
 
-	MissileReloadCountdown -= delta;
-	if (MissileReloadCountdown <= 0)
-	{ 
-	  _serilogger.Debug($"PlayerShip.cs: player {uuid} missile reload countdown complete");
-	  MissileReady = true;
-	}
+    MissileReloadCountdown -= delta;
+    if (MissileReloadCountdown <= 0)
+    { 
+      _serilogger.Debug($"PlayerShip.cs: player {uuid} missile reload countdown complete");
+      MissileReady = true;
+    }
   }
 
   public void UpdateFocused()
   {
-	if (isFocused) shipSprite.Modulate = new Color(4,4,4,1);
-	else shipSprite.Modulate = new Color(1,1,1,1);
+    if (isFocused) shipSprite.Modulate = new Color(4,4,4,1);
+    else shipSprite.Modulate = new Color(1,1,1,1);
   }
 
   public override void _Process(double delta)
   {
 
-	if (QueuedForRemoval) return;
+    if (QueuedForRemoval) return;
 
-	if (shipThing == null) shipThing = (Node2D)GetParent();
+    if (shipThing == null) shipThing = (Node2D)GetParent();
 
-	// figure out the hex from the pixel position
-	FractionalHex theHex = theLayout.PixelToHex(new Point(GlobalPosition.X, GlobalPosition.Y));
-	hexLabel.Text = $"q: {theHex.HexRound().q}, r: {theHex.HexRound().r}, s: {theHex.HexRound().s}";
+    // figure out the hex from the pixel position
+    FractionalHex theHex = theLayout.PixelToHex(new Point(GlobalPosition.X, GlobalPosition.Y));
+    hexLabel.Text = $"q: {theHex.HexRound().q}, r: {theHex.HexRound().r}, s: {theHex.HexRound().s}";
 
-	angularVelocityLabel.Text = $"Rot: {RotationDegrees}";
-	linearVelocityLabel.Text = $"Vel: {CurrentVelocity}";
-	hitPointsLabel.Text = $"HP: {HitPoints}";
-	positionLabel.Text = $"X: {GlobalPosition.X} Y: {GlobalPosition.Y}";
+    angularVelocityLabel.Text = $"Rot: {RotationDegrees}";
+    linearVelocityLabel.Text = $"Vel: {CurrentVelocity}";
+    hitPointsLabel.Text = $"HP: {HitPoints}";
+    positionLabel.Text = $"X: {GlobalPosition.X} Y: {GlobalPosition.Y}";
 
-	CheckMissileReload(delta);
-	UpdateFocused();
+    CheckMissileReload(delta);
+    UpdateFocused();
   }
   public override void _PhysicsProcess(double delta)
   {
-	  // //GD.Print("PlayerShip::_PhysicsProcess");
-	if (shipThing == null) shipThing = (Node2D)GetParent();
+      // //GD.Print("PlayerShip::_PhysicsProcess");
+    if (shipThing == null) shipThing = (Node2D)GetParent();
 
-	// somewhat based on: https://kidscancode.org/godot_recipes/2d/topdown_movement/
-	// "rotate and move" / asteroids-style-ish
+    // somewhat based on: https://kidscancode.org/godot_recipes/2d/topdown_movement/
+    // "rotate and move" / asteroids-style-ish
 
-	double rotation_dir = 0; // in case we need it
+    double rotation_dir = 0; // in case we need it
 
-	_serilogger.Verbose($"{uuid}: handling physics");
-	Vector2 thisMovement = Vector2.Zero;
-	if (MovementQueue.TryDequeue(out thisMovement))
-	{
-	  _serilogger.Verbose($"UUID: {uuid} X: {thisMovement.X} Y: {thisMovement.Y}");
+    _serilogger.Verbose($"{uuid}: handling physics");
+    Vector2 thisMovement = Vector2.Zero;
+    if (MovementQueue.TryDequeue(out thisMovement))
+    {
+      _serilogger.Verbose($"UUID: {uuid} X: {thisMovement.X} Y: {thisMovement.Y}");
 
-	  if (thisMovement.Y > 0)
-	  {
-		CurrentVelocity = Mathf.Lerp(CurrentVelocity, MaxSpeed, Thrust * delta);
+      if (thisMovement.Y > 0)
+      {
+        CurrentVelocity = Mathf.Lerp(CurrentVelocity, MaxSpeed, Thrust * delta);
 
-		// max out speed when velocity gets above threshold for same reason
-		if (CurrentVelocity > MaxSpeed * (GoThreshold/100)) { CurrentVelocity = MaxSpeed; }
-	  }
+        // max out speed when velocity gets above threshold for same reason
+        if (CurrentVelocity > MaxSpeed * (GoThreshold/100)) { CurrentVelocity = MaxSpeed; }
+      }
 
-	  if (thisMovement.Y < 0)
-	  {
-		CurrentVelocity = Mathf.Lerp(CurrentVelocity, 0, Thrust * delta);
+      if (thisMovement.Y < 0)
+      {
+        CurrentVelocity = Mathf.Lerp(CurrentVelocity, 0, Thrust * delta);
 
-		// cut speed when velocity gets below threshold, otherwise LERPing
-		// results in never actually stopping. 
-		if (CurrentVelocity < MaxSpeed * (StopThreshold/100)) { CurrentVelocity = 0; }
-	  }
+        // cut speed when velocity gets below threshold, otherwise LERPing
+        // results in never actually stopping. 
+        if (CurrentVelocity < MaxSpeed * (StopThreshold/100)) { CurrentVelocity = 0; }
+      }
 
-	  if (thisMovement.X != 0)
-	  {
-		rotation_dir = thisMovement.X;
-	  }
+      if (thisMovement.X != 0)
+      {
+        rotation_dir = thisMovement.X;
+      }
 
-	  _serilogger.Verbose($"UUID: {uuid} Velocity: {CurrentVelocity}");
-	}
-	
-	Vector2 velocity =  -(Transform.Y * (float)CurrentVelocity);
-	_serilogger.Verbose($"UUID: {uuid} Vector X: {velocity.X} Y: {velocity.Y} ");
-	Rotation += (float)(rotation_dir * RotationThrust * delta);
+      _serilogger.Verbose($"UUID: {uuid} Velocity: {CurrentVelocity}");
+    }
+    
+    Vector2 velocity =  -(Transform.Y * (float)CurrentVelocity);
+    _serilogger.Verbose($"UUID: {uuid} Vector X: {velocity.X} Y: {velocity.Y} ");
+    Rotation += (float)(rotation_dir * RotationThrust * delta);
 
-	// TODO: implement collision mechanics
-	MoveAndCollide(velocity);
+    // TODO: implement collision mechanics
+    MoveAndCollide(velocity);
 
-	// TODO: need to adust the clamp when the starfield is lopsided in the early
-	// game
+    // TODO: need to adust the clamp when the starfield is lopsided in the early
+    // game
 
-	// clamp the player to the starfield radius
-	Int32 starFieldRadiusPixels = MyServer.StarFieldRadiusPixels;
-	Vector2 currentGlobalPosition = GlobalPosition;
-	if (currentGlobalPosition.Length() > starFieldRadiusPixels)
-	{
-	  Vector2 newPosition = starFieldRadiusPixels * currentGlobalPosition.Normalized();
-	  GlobalPosition = newPosition;
-	}
+    // clamp the player to the starfield radius
+    Int32 starFieldRadiusPixels = MyServer.StarFieldRadiusPixels;
+    Vector2 currentGlobalPosition = GlobalPosition;
+    if (currentGlobalPosition.Length() > starFieldRadiusPixels)
+    {
+      Vector2 newPosition = starFieldRadiusPixels * currentGlobalPosition.Normalized();
+      GlobalPosition = newPosition;
+    }
   }
 }
--- SpaceMissile.cs
+++ SpaceMissile.cs
@@ -24,100 +24,100 @@ public partial class SpaceMissile : Area2D
 
   public GameEvent CreateMissileGameEventBuffer(GameEvent.GameEventType BufferType, String OwnerUUID)
   {
-	GameEvent gameEvent = new GameEvent();
-	//egeb.Type = BufferType;
-	gameEvent.game_event_type = BufferType;
+    GameEvent gameEvent = new GameEvent();
+    //egeb.Type = BufferType;
+    gameEvent.game_event_type = BufferType;
 
-	//egeb.objectType = EntityGameEventBuffer.EntityGameEventBufferObjectType.Missile;
-	gameEvent.game_object_type = GameEvent.GameObjectType.GameObjectTypeMissile;
+    //egeb.objectType = EntityGameEventBuffer.EntityGameEventBufferObjectType.Missile;
+    gameEvent.game_object_type = GameEvent.GameObjectType.GameObjectTypeMissile;
 
-	//egeb.Uuid = uuid;
-	gameEvent.Uuid = uuid;
+    //egeb.Uuid = uuid;
+    gameEvent.Uuid = uuid;
 
-	//Box2d.PbBody body = new Box2d.PbBody();
-	//body.Type = Box2d.PbBodyType.Kinematic; // not sure if this should maybe be static
+    //Box2d.PbBody body = new Box2d.PbBody();
+    //body.Type = Box2d.PbBodyType.Kinematic; // not sure if this should maybe be static
 
-	// need to use the GlobalPosition because the ship node ends up being offset
-	// from the parent Node2D
-	//body.Position = new Box2d.PbVec2 
-	//  { 
-	//    X = GlobalPosition.x,
-	//    Y = GlobalPosition.y
-	//  };
-	gameEvent.PositionX = (int)GlobalPosition.X;
-	gameEvent.PositionY = (int)GlobalPosition.Y;
+    // need to use the GlobalPosition because the ship node ends up being offset
+    // from the parent Node2D
+    //body.Position = new Box2d.PbVec2 
+    //  { 
+    //    X = GlobalPosition.x,
+    //    Y = GlobalPosition.y
+    //  };
+    gameEvent.PositionX = (int)GlobalPosition.X;
+    gameEvent.PositionY = (int)GlobalPosition.Y;
 
-	//body.Angle = RotationDegrees;
-	gameEvent.Angle = RotationDegrees;
+    //body.Angle = RotationDegrees;
+    gameEvent.Angle = RotationDegrees;
 
-	//body.AbsoluteVelocity = CurrentVelocity;
-	gameEvent.AbsoluteVelocity = MissileSpeed;
+    //body.AbsoluteVelocity = CurrentVelocity;
+    gameEvent.AbsoluteVelocity = MissileSpeed;
 
-	gameEvent.OwnerUuid = OwnerUUID;
+    gameEvent.OwnerUuid = OwnerUUID;
 
-	return gameEvent;
+    return gameEvent;
   }
   
   // Called when the node enters the scene tree for the first time.
   public override void _Ready() 
   {  
-	// initialize the logging configuration
-	MyServer = GetNode<Server>("/root/Server");
-	_serilogger = MyServer._serilogger;
+    // initialize the logging configuration
+    MyServer = GetNode<Server>("/root/Server");
+    _serilogger = MyServer._serilogger;
 
-	// add the missile to the missiles group so that we can iterate over
-	// the entire group and send updates later
-	AddToGroup("missiles");
+    // add the missile to the missiles group so that we can iterate over
+    // the entire group and send updates later
+    AddToGroup("missiles");
   }
 
   public override void _PhysicsProcess(double delta)
   {
-	// TODO disable the collision shape until the missile is "away" from the ship
+    // TODO disable the collision shape until the missile is "away" from the ship
 
-	// create a new vector and rotate it by the current heading of the missile
-	// then move the missile in the direction of that vector
-	Vector2 velocity = new Vector2(0, -1);
-	velocity = velocity.Rotated(Rotation);
-	velocity = velocity * (float)MissileSpeed * (float)delta;
-	Position += velocity;
+    // create a new vector and rotate it by the current heading of the missile
+    // then move the missile in the direction of that vector
+    Vector2 velocity = new Vector2(0, -1);
+    velocity = velocity.Rotated(Rotation);
+    velocity = velocity * (float)MissileSpeed * (float)delta;
+    Position += velocity;
 
-	// once the life reaches zero, remove the missile and don't forget
-	// to expire it from the parent's perspective
-	MissileLife -= (float)delta;
-	if (MissileLife <= 0) { 
-	  QueueFree(); 
+    // once the life reaches zero, remove the missile and don't forget
+    // to expire it from the parent's perspective
+    MissileLife -= (float)delta;
+    if (MissileLife <= 0) { 
+      QueueFree(); 
 
-	  // there's got to be a better way
-	  MyPlayer.ExpireMissile();
-	}
+      // there's got to be a better way
+      MyPlayer.ExpireMissile();
+    }
   }
 
   void _onSpaceMissileBodyEntered(Node body)
   {
-	_serilogger.Debug("SpaceMissile.cs: Body entered!");
+    _serilogger.Debug("SpaceMissile.cs: Body entered!");
 
-	if (body.GetType().Name != "PlayerShip")
-	{
-	  // We didn't hit another player, so remove ourselves, expire the missile, and return
-	  // TODO: may want to decide to do something fancy here
-	  QueueFree();
-	  MyPlayer.ExpireMissile();
-	  return;
-	}
+    if (body.GetType().Name != "PlayerShip")
+    {
+      // We didn't hit another player, so remove ourselves, expire the missile, and return
+      // TODO: may want to decide to do something fancy here
+      QueueFree();
+      MyPlayer.ExpireMissile();
+      return;
+    }
 
-	// We hit another Player, so proceed
-	// EmitSignal("Hit", (PlayerShip)body);
-	_HandleHit((PlayerShip)body);
+    // We hit another Player, so proceed
+    // EmitSignal("Hit", (PlayerShip)body);
+    _HandleHit((PlayerShip)body);
 
-	// Must be deferred as we can't change physics properties on a physics callback.
-	GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred("disabled", true);
+    // Must be deferred as we can't change physics properties on a physics callback.
+    GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred("disabled", true);
   }
 
   void _HandleHit(PlayerShip HitPlayer)
   {
-	_serilogger.Debug("SpaceMissile.cs: Evaluating hit!");
-	QueueFree();
-	MyPlayer.ExpireMissile();
-	HitPlayer.TakeDamage(MissileDamage);
+    _serilogger.Debug("SpaceMissile.cs: Evaluating hit!");
+    QueueFree();
+    MyPlayer.ExpireMissile();
+    HitPlayer.TakeDamage(MissileDamage);
   }
 }
